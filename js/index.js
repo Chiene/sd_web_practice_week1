3a9ef75d88c8229638cd6daead7e31cdd2eee41e
Jia-ching Jian <sagiters1128@gmail.com>
2015-07-11 20:29:54 +0800

    test js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict"  
+// var column_img = document.getElementsByClassName('column-img');
+// for (var i = column_img.length - 1; i >= 0; i--) {
+//    column_img[i].getElementsByTag('label').innerHTML = "hello";
+//    console.log(column_img[i].getElementsByTag('label'));
+//  }; 
+console.log("hello");
