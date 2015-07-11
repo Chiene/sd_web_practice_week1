00cb5a9fb8a7ed8247ef3aa0350e59194dc1bb09
Jia-ching Jian <sagiters1128@gmail.com>
2015-07-11 22:16:13 +0800

    Change the image after click

--- js/index.js
+++ js/index.js
@@ -1,7 +1,40 @@
 "use strict"  
-// var column_img = document.getElementsByClassName('column-img');
-// for (var i = column_img.length - 1; i >= 0; i--) {
-//    column_img[i].getElementsByTag('label').innerHTML = "hello";
-//    console.log(column_img[i].getElementsByTag('label'));
-//  }; 
-console.log("hello");
+document.addEventListener("DOMContentLoaded", function(event) {
+var theParent = document.querySelector("img");
+console.log(theParent);
+theParent.addEventListener("click", doSomething, false);
+ 
+function doSomething(e) {
+    
+}
+
+
+var images = document.getElementsByTagName("img");
+for (var i = images.length - 1; i >= 0; i--) {
+  var img = images[i];
+  img.addEventListener("click", function(e) {
+  var target = e.target;
+  var value = target.getAttribute('value');
+  if(value == 0) {
+    target.src = "./images/"+target.getAttribute('id')+"1.jpg";
+    target.setAttribute('value','1');
+    //target.value = 1;
+    console.log(value);
+    
+  }
+  else {
+    target.src = "./images/"+target.getAttribute('id')+".jpg";
+    target.setAttribute('value','0');
+    console.log(value);
+  }
+
+ },false);
+};
+
+
+});
+
+function changeCatImage (cat_id) {
+  
+}
+
