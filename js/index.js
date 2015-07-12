fa7459a39e61ed0cf5dddd34355972e35ea36d0b
Jia-ching Jian <sagiters1128@gmail.com>
2015-07-12 16:35:38 +0800

    modify js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,44 @@
 "use strict"  
-document.addEventListener("DOMContentLoaded", function(event) {
 
-var images = document.getElementsByTagName("img");
-for (var i = images.length - 1; i >= 0; i--) {
-  var img = images[i];
-  img.addEventListener("mousedown", function(e) {
-    var target = e.target;
-    var value = target.getAttribute('value');
-    target.src = "./images/"+target.getAttribute('id')+"1.jpg";
-    target.setAttribute('value','1');
-   },false);
-  img.addEventListener("mouseup", function(e) {
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var img;
+  var button = document.getElementsByClassName("button")[0];
+  var images = document.getElementsByTagName("img");
+  button.addEventListener("click",feed,false);
+  // init img
+  for (var i = images.length - 1; i >= 0; i--) {
+    img = images[i];
+    img.addEventListener("mousedown", up,false);
+    img.addEventListener("mouseup", down,false);
+  }
+});
+
+function up(e) {
+  var target = e.target;
+  var value = target.getAttribute('value');
+  target.src = "./images/"+target.getAttribute('id')+"1.jpg";
+  target.setAttribute('value','1');
+}
+function down(e) {
   var target = e.target;
   var value = target.getAttribute('value');
   target.src = "./images/"+target.getAttribute('id')+".jpg";
   target.setAttribute('value','0');
- },false);
-  
 }
-var button = document.getElementsByClassName("button")[0];
-button.addEventListener("click",feed,false);
-});
-//'3px solid #DDD'
-//'3px solid rgba(42, 242, 42, 1)'
-//checked = document.querySelector('input[name="selectCat"]:checked').value;
+
 function feed () {
   var results ;
   var result_class;
   var className = document.getElementsByClassName('text')[0].value;
   var idName = document.querySelector('input[name="selectCat"]:checked').value;
   var cats = document.getElementsByClassName("cat");
-  // init
-  
+  // init 
   for (var i = cats.length - 1; i >= 0; i--) {
-  cats[i].style.border = '3px solid #DDD';
+    cats[i].style.border = '3px solid #DDD';
   };
-
-
   if (className == '' ) { results = cats;}
   else { results = document.getElementsByClassName(className); };
-
-  
   switch(idName) {
     case "All":
       for (var i = results.length - 1; i >= 0; i--) {
@@ -53,7 +51,6 @@ function feed () {
       if(result_class.indexOf(className) > -1) {
        results.style.border = '3px solid rgba(42, 242, 42, 1)';
       }
-
       break;
   }
 }
