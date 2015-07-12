07042cb8a1fc5139a083dd28dc38849af47cff2b
Jia-ching Jian <sagiters1128@gmail.com>
2015-07-12 16:26:13 +0800

    complete and modify the search result

--- js/index.js
+++ js/index.js
@@ -4,58 +4,58 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var images = document.getElementsByTagName("img");
 for (var i = images.length - 1; i >= 0; i--) {
   var img = images[i];
-  img.addEventListener("click", function(e) {
-  var target = e.target;
-  var value = target.getAttribute('value');
-  if(value == 0) {
+  img.addEventListener("mousedown", function(e) {
+    var target = e.target;
+    var value = target.getAttribute('value');
     target.src = "./images/"+target.getAttribute('id')+"1.jpg";
     target.setAttribute('value','1');
-  }
-  else {
-    target.src = "./images/"+target.getAttribute('id')+".jpg";
-    target.setAttribute('value','0');
-  }
+   },false);
+  img.addEventListener("mouseup", function(e) {
+  var target = e.target;
+  var value = target.getAttribute('value');
+  target.src = "./images/"+target.getAttribute('id')+".jpg";
+  target.setAttribute('value','0');
  },false);
+  
 }
 var button = document.getElementsByClassName("button")[0];
 button.addEventListener("click",feed,false);
-console.dir(button);
-
-
 });
+//'3px solid #DDD'
+//'3px solid rgba(42, 242, 42, 1)'
+//checked = document.querySelector('input[name="selectCat"]:checked').value;
 function feed () {
-  // init
+  var results ;
+  var result_class;
+  var className = document.getElementsByClassName('text')[0].value;
+  var idName = document.querySelector('input[name="selectCat"]:checked').value;
   var cats = document.getElementsByClassName("cat");
+  // init
+  
   for (var i = cats.length - 1; i >= 0; i--) {
-    cats[i].style.border = '3px solid #DDD';
+  cats[i].style.border = '3px solid #DDD';
   };
 
-  var inputText = document.getElementsByClassName("text")[0];
-  var select_category = document.getElementsByClassName(inputText.value);
-  //class
-  for (var i = select_category.length - 1; i >= 0; i--) { 
-    select_category[i].style.border = '3px solid rgba(42, 242, 42, 1)';
-    //
-  }
-  // id
-  var checked = document.querySelector('input[name="selectCat"]:checked').value;
-  console.dir(checked);
 
-  switch(checked) {
+  if (className == '' ) { results = cats;}
+  else { results = document.getElementsByClassName(className); };
+
+  
+  switch(idName) {
     case "All":
-      for (var i = cats.length - 1; i >= 0; i--) {
-        cats[i].style.border = '3px solid rgba(42, 242, 42, 1)';
-      };
-      break;
-    case "None":
+      for (var i = results.length - 1; i >= 0; i--) {
+        results[i].style.border = '3px solid rgba(42, 242, 42, 1)';
+      }
       break;
     default:
-      document.getElementById(checked).style.border = '3px solid rgba(42, 242, 42, 1)';
+      results = document.getElementById(idName);
+      result_class = results.className;
+      if(result_class.indexOf(className) > -1) {
+       results.style.border = '3px solid rgba(42, 242, 42, 1)';
+      }
+
       break;
-  } 
- 
-
-
+  }
 }
 
 
